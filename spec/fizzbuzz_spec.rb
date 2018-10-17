7eb929e552daf8f03b83c046fa01f4a4c7fe96ac
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-17 01:52:49 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,12 +2,12 @@ require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 
 def fizzbuzz(x)
-  if (x % 3 == 0 && x % 5 == 0)
-  "fizzbuzz"
+  if(x % 3 == 0 && x % 5 == 0)
+  "Fizzbuzz"
 elsif (x % 5 == 0)
-  "buzz"
+  "Buzz"
 elsif (x % 3 == 0)  
-  "fizz"
+  "Fizz"
 else 
    "nil"
   end
