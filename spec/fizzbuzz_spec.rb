11600ba40743da3107f0f0cd6ce19825288df41c
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-17 01:54:31 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,10 +4,10 @@ require_relative '../fizzbuzz.rb'
 def fizzbuzz(x)
   if(x%3==0&&x%5==0)
   "Fizzbuzz"
-elsif (x%5==0)
-  "Buzz"
-elsif (x%3==0)  
+elsif (x%3==0)
   "Fizz"
+elsif (x%5==0)  
+  "Buzz"
   end
 end
 
